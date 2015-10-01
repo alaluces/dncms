7ddba27fb6fdbb442c18977b27d24bec7c0d3725
aries_laluces@yahoo.com <>
2015-10-01 09:05:14 +0800

    first commit

---
+++ app/controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+class UserController extends BaseController {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |	Route::get('/', 'HomeController@showWelcome');
+    |
+    */
+    
+    public function __construct()
+    {
+        $this->beforeFilter('auth', array('except' => 'getLogin'));
+    }    
+
+    public function showWelcome()
+    {
+            return View::make('hello');
+    }
+    
+    public function getLogin()
+    {
+        echo 'login';
+    }
+    
+    public function showHome()
+    {
+        echo 'home';
+
+    }          
+
+}
